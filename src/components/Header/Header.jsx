7315221f9da24fa05e642644fddc7add206b53ee
Shekhar Sharma <beingperfect76@gmail.com>
2024-06-13 09:04:34 +0530

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+
+export default function Header() {
+	return (
+		<div className="h-fit border-neutral-600 text-white flex items-center justify-between px-28 pt-8">
+			<Link href={"/"}>
+				<span className="text-6xl font-bold text-white">
+					S<span className="text-[#F4C430]">.</span>
+				</span>
+			</Link>
+			<div className="flex w-1/3 justify-between font-medium text-md items-center">
+				<Link href={"/"}>
+					<span className="cursor-pointer font-medium hover:text-[#F4C430]">
+						Home
+					</span>
+				</Link>
+				<span className="cursor-pointer font-medium hover:text-[#F4C430]">
+					Projects
+				</span>
+				<Link href={"/education"}>
+					<span className="cursor-pointer font-medium hover:text-[#F4C430]">
+						Education
+					</span>
+				</Link>
+				<span className="cursor-pointer font-medium hover:text-[#F4C430]">
+					Contact
+				</span>
+			</div>
+			{/* <span className="cursor-pointer font-medium hover:text-[#F4C430] bg-red-900 p-1 px-4 rounded-md border border-neutral-400">
+					Resume
+				</span> */}
+			<a
+				href="/Shekhar_Sharma_Experienced_2024.pdf"
+				target="_blank"
+				download="Shekhar_Sharma_Experienced_2024"
+			>
+				<button className="button" type="button">
+					<span className="button__text">Resume</span>
+					<span className="button__icon">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 35 35"
+							id="bdd05811-e15d-428c-bb53-8661459f9307"
+							data-name="Layer 2"
+							className="svg"
+						>
+							<path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+							<path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+							<path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+						</svg>
+					</span>
+				</button>
+			</a>
+		</div>
+	);
+}
