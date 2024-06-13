d9e19a624ca7ead2ef7a1082cb15120797cdddc7
Shekhar Sharma <beingperfect76@gmail.com>
2024-06-13 09:07:22 +0530

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+export default function Footer() {
+	return (
+		<div className="h-16 border-t border-neutral-600  text-white flex items-center justify-center">
+			<span className="text-sm">
+				© 2024 Shekhar Sharma. All Rights Reserved.
+			</span>
+		</div>
+	);
+}
